a053b852e774f37a3302fb2ff18c511a99a23e3e
Cristian Cortez <gonzalocortez@dyn239205.cc.lehigh.edu>
2025-03-01 21:12:15 -0500

    Added new card component that has speech bubble, modified gamescene

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,56 +1,4 @@
-/*import Phaser from 'phaser';
-import CardComponent from '../components/CardComponent';
 
-class GameScene extends Phaser.Scene {
-    constructor() {
-        super('GameScene');
-        this.stats = {
-            health: 100,
-            money: 100,
-            skills: 100,
-            daysRemaining: 60
-        };
-    }
-
-    preload() {
-        // Load any necessary assets here
-    }
-
-    create() {
-        this.add.text(400, 50, 'Game Scene', { fontSize: '32px', fill: '#000' }).setOrigin(0.5); // Change fill color to black
-        this.showScenario();
-    }
-
-    showScenario() {
-
-    }
-
-    updateStats(statsChange) {
-        this.stats.health += statsChange.health;
-        this.stats.money += statsChange.money;
-        this.stats.skills += statsChange.skills;
-        this.stats.daysRemaining -= 1;
-
-        console.log('Updated Stats:', this.stats);
-    }
-
-    nextScenario() {
-        // Logic to show the next scenario or end the game
-        if (this.stats.daysRemaining <= 0) {
-            this.scene.start('EndScene');
-        } else {
-            this.showScenario();
-        }
-    }
-
-    endGame() {
-        // Evaluate stats and transition to EndScene
-        this.scene.start('EndScene', { health: this.stats.health, money: this.stats.money, skills: this.stats.skills });
-    }
-}
-
-export default GameScene;
-*/
 import Phaser from 'phaser';
 import CardComponent2 from '../components/CardComponent2';
 import { animateText } from '../utils/typeWriter';
@@ -69,6 +17,7 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('job_background', 'assets/images/JobSceneBackground.png');
         this.load.image('closeup', 'assets/images/CloseUp.png');
+        this.load.image('textbox', 'assets/images/TextBox.png');
     }
 
     create() {
@@ -84,9 +33,9 @@ class GameScene extends Phaser.Scene {
         const popupTexts = [popup1, popup2, popup3];
 
         const options = [
-            { text: "Network", statsChange: { health: -10, money: -10, skills: +20 }, info: "Luckily, you have many connections from your last job that can help you find a new one."},
-            { text: "Cold Apply", statsChange: { health: +20, money: -20, skills: 0 }, info: "With your great experience, it should be no problem for you to cold apply to new positions."},
-            { text: "Self Petition Visa", statsChange: { health: +20, money: -20, skills: 0 }, info: "You have many great accomplishments and are at the top of your field. Maybe you should try to apply for a self position VISA? With this, you would not need an employer to sponsor you."},
+            { text: "Network", statsChange: { health: -10, money: -10, skills: +20 }, info: "Thankfully, I have many connections from my last job! Hopefully I can get a job from them..."},
+            { text: "Cold Apply", statsChange: { health: +20, money: -20, skills: 0 }, info: "With my experience, I'm sure I could just cold apply and get a new job!"},
+            { text: "Self Petition Visa", statsChange: { health: +20, money: -20, skills: 0 }, info: "I do have many great accomplishments in research and my field, so this VISA should not be too complicated to get..."},
         ];
         
         new CardComponent2(this, 
